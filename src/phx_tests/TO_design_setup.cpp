fff8156d7e360dec3e8ece83c078daf00dc3311a
LuningFang <lfang9@wisc.edu>
2024-11-14 12:44:51 -0600

    update TO design setup

--- src/phx_tests/TO_design_setup.cpp
+++ src/phx_tests/TO_design_setup.cpp
@@ -32,9 +32,15 @@ int AddParticles(DEMSolver& DEMSim,
 
 int main(int argc, char** argv) {
 
-    double bxDim = 0.5;
-    double byDim = 5.0; // change this to 5 for the actual test 
-    double bzDim = 34;
+    // double bxDim = 0.5;
+    // double byDim = 5.0; // change this to 5 for the actual test 
+    // double bzDim = 34;
+
+    // double bxDim = 5.0;
+    double bxDim = 1.0;
+    double byDim = 48.0;
+    double bzDim = 0.5;
+
 
     if (argc != 2){
         std::cout << "Usage: ./TO_design_setup <Test ID>" << std::endl;
@@ -63,11 +69,11 @@ int main(int argc, char** argv) {
 
     float fric_coef = 0.6;
     float carbo_density = 3.6;
-    double scaling = 0.1;  // for testing, actual particle scale is 0.1
+    double scaling = 0.2;  // for testing, actual particle scale is 0.1
     std::vector<double> radius_array = {0.212 * scaling, 0.2 * scaling, 0.178 * scaling};
 
-    float step_size = 2.5e-6;
-    float time_end = 5.0;
+    float step_size = 5e-6;
+    float time_end = 2.0;
     unsigned int fps = 100;
 
     // extract test name from TO_design_mesh
@@ -75,20 +81,22 @@ int main(int argc, char** argv) {
     auto mat_type_wall = DEMSim.LoadMaterial({{"E", 2e7}, {"nu", 0.3}, {"CoR", 0.6}, {"mu", fric_coef}, {"Crr", 0.0}});
 
     auto TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
+    TO_mesh->Move(make_float3(0,-3,0), make_float4(0, 0, 0, 1));
+
     std::cout << TO_mesh->GetNumTriangles() << " faces and " << TO_mesh->GetNumNodes() << " vertices" << std::endl;
 
-    TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
-    TO_mesh->Mirror(make_float3(0, 1, 0), make_float3(0, 1, 0));
+    // TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
+    // TO_mesh->Mirror(make_float3(0, 1, 0), make_float3(0, 1, 0));
 
-    TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
-    TO_mesh->Move(make_float3(0,2,0), make_float4(0, 0, 0, 1));
+    // TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
+    // TO_mesh->Move(make_float3(0,2,0), make_float4(0, 0, 0, 1));
 
-    TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
-    TO_mesh->Mirror(make_float3(0, 1, 0), make_float3(0, 1, 0));
-    TO_mesh->Move(make_float3(0,2,0), make_float4(0, 0, 0, 1));
+    // TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
+    // TO_mesh->Mirror(make_float3(0, 1, 0), make_float3(0, 1, 0));
+    // TO_mesh->Move(make_float3(0,2,0), make_float4(0, 0, 0, 1));
 
-    TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
-    TO_mesh->Move(make_float3(0,4,0), make_float4(0, 0, 0, 1));
+    // TO_mesh = DEMSim.AddWavefrontMeshObject(GetDEMEDataFile(TO_design_mesh), mat_type_wall);
+    // TO_mesh->Move(make_float3(0,4,0), make_float4(0, 0, 0, 1));
 
 
 
@@ -99,9 +107,9 @@ int main(int argc, char** argv) {
     DEMSim.SetFamilyFixed(family_ID);
 
     // Add sim domain
-    DEMSim.InstructBoxDomainDimension({ 0.,          bxDim}, 
-                                      { 0.,          byDim},
-                                      {-bzDim     , bzDim/  2.});
+    DEMSim.InstructBoxDomainDimension({-bxDim / 2., bxDim / 2.}, 
+                                      {-21, byDim},
+                                      {-bzDim / 2., bzDim/  2.});
     DEMSim.InstructBoxDomainBoundingBC("all", mat_type_wall);
 
 
@@ -121,8 +129,8 @@ int main(int argc, char** argv) {
 
     // sampler spacing
     PDSampler smapler(2.02 * radius_array[0]);
-    float3 sampler_center = make_float3(bxDim/2., byDim/2., -bzDim/2.);
-    float3 sampler_hdim = make_float3(bxDim/2. * 0.95, byDim/2. * 0.95, bzDim/2. * 0.9);
+    float3 sampler_center = make_float3(0, byDim/2. - 6, 0);
+    float3 sampler_hdim = make_float3(bxDim/2., byDim/4. , bzDim/2.);
     particle_xyz = smapler.SampleBox(sampler_center, sampler_hdim);
 
     unsigned int num_clumps = particle_xyz.size();
@@ -134,7 +142,7 @@ int main(int argc, char** argv) {
 
 
     DEMSim.SetInitTimeStep(step_size);
-    DEMSim.SetGravitationalAcceleration(make_float3(0, 0, 981));
+    DEMSim.SetGravitationalAcceleration(make_float3(0, -981, 0));
     DEMSim.SetExpandSafetyType("auto");
     DEMSim.Initialize();
 
